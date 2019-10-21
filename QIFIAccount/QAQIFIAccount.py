aab03a219f14b29f3cfd7f0bb8029468b3195a4a
yutiansut <yutiansut@qq.com>
2019-10-21 13:13:06 +0800

    #update

--- QIFIAccount/QAQIFIAccount.py
+++ QIFIAccount/QAQIFIAccount.py
@@ -207,7 +207,7 @@ class QIFI_Account():
 
     @property
     def dtstr(self):
-        return str(datetime.datetime.now()).replace('.', '-')
+        return str(datetime.datetime.now()).replace('.', '_')
 
     def ask_deposit(self, money):
 
@@ -364,7 +364,7 @@ class QIFI_Account():
     def transform_dt(self, times):
         if isinstance(times, str):
             tradedt = datetime.datetime.strptime(times, '%Y-%m-%d %H:%M:%S') if len(
-                times) == 19 else datetime.datetime.strptime(times.replace('-', '.'), '%Y-%m-%d %H:%M:%S.%f')
+                times) == 19 else datetime.datetime.strptime(times.replace('_', '.'), '%Y-%m-%d %H:%M:%S.%f')
             return tradedt.timestamp()*1000000000
         elif isinstance(times, datetime.datetime):
             return tradedt.timestamp()*1000000000
