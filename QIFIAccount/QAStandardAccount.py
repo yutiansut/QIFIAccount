db28575eafe777bbbbc2e0bf8e332b292fde4f29
yutiansut <yutiansut@qq.com>
2020-08-03 01:53:31 +0800

    #

--- QIFIAccount/QAStandardAccount.py
+++
@@ -1,3 +0,0 @@
-class QIFI_StandardAccount():
-    def __init__(self):
-        pass
--- QIFIAccount/__init__.py
+++ QIFIAccount/__init__.py
@@ -1,6 +1,6 @@
 from QIFIAccount.QAQIFIAccount import QIFI_Account, ORDER_DIRECTION
 from QIFIAccount.QAPosition import QA_Position
 from QIFIAccount.QARealtimeStockSim import QIFI_StockSIM_Account
-from QIFIAccount.QAStandardAccount import QIFI_StandardAccount
+from QIFIAccount.QIFIPortfolio import QIFI_Portfolio
 __version__ = '1.8'
 __author__ = 'yutiansut'
