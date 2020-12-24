d364ca9069d5444c2d27a26e472bfb23bc70b41b
yutiansut <yutiansut@qq.com>
2020-12-25 02:23:56 +0800

    #update for traceback

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
     ],
-    install_requires=['quantaxis', 'qaenv', 'click', 'quantaxis_pubsub', 'traceback', 'pytest'],
+    install_requires=['quantaxis', 'qaenv', 'click', 'quantaxis_pubsub', 'pytest'],
     # install_requires=requirements,
     entry_points={
         'console_scripts': [
